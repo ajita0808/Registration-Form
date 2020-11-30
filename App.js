e5948e3288c13795b9e372c7ad7db0b78329a23d
Ajita Mishra <66466673+ajita0808@users.noreply.github.com>
2020-11-30 20:22:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import RegistrationForm from './registrationform'
+class App extends Component{
+  render(){
+    return(
+      <RegistrationForm/>
+    );
+  }
+}
+export default App;
